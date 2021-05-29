e037536d4d696f4d67960602c9e5ef8166b7bdb6
Wojciech Szlosek <wojciech185sz@gmail.com>
2021-05-29 13:03:26 +0200

    add histogram

--- main.py
+++ main.py
@@ -3,10 +3,7 @@
 import math
 import matplotlib.pyplot as plt
 import numpy
-import statistics
-import numpy as np
-import scipy.stats as st
-from statsmodels.sandbox.stats.runs import runstest_1samp
+
 
 class RandomNumberGenerator:
 
@@ -23,13 +20,12 @@ class RandomNumberGenerator:
 class UniformNumberGenerator(RandomNumberGenerator):
 
     def __next__(self, a=0, b=1):
-
         if a == 0 and b == 1:
-            return super(UniformNumberGenerator, self).__next__()/self.n
+            return super(UniformNumberGenerator, self).__next__() / self.n
 
-        lxy = b-a+1
+        lxy = b - a + 1
 
-        return ((super(UniformNumberGenerator, self).__next__()/self.n)*lxy + a)
+        return ((super(UniformNumberGenerator, self).__next__() / self.n) * lxy + a)
 
 
 class Bernoulli:
@@ -42,6 +38,7 @@ class Bernoulli:
             return 1
         else:
             return 0
+
 
 class Binomial:
 
@@ -70,7 +67,7 @@ class Poisson:
             w *= self.u.__next__()
             x += 1
 
-        return x-1
+        return x - 1
 
 
 class Exponential:
@@ -78,14 +75,13 @@ class Exponential:
     def __init__(self):
         self.u = UniformNumberGenerator()
 
-
     def run(self):
-
         u1 = self.u.__next__()
         return -math.log(u1, math.e)
 
 
 class Normal:
+
     def __init__(self):
         self.u = UniformNumberGenerator()
         self.v = UniformNumberGenerator()
@@ -94,10 +90,10 @@ class Normal:
     def run(self):
 
         w1 = self.u.__next__()
-        p1 = 16/((2*math.pi*math.e)**0.5)
+        p1 = 16 / ((2 * math.pi * math.e) ** 0.5)
         p2 = 0.1108179673
         p4 = 0.0026997960
-        p3 = 1-p1-p2-p4
+        p3 = 1 - p1 - p2 - p4
         c1 = 17.4973119
         c2 = 4.73570326
         c3 = 2.15787544
@@ -105,36 +101,36 @@ class Normal:
         M = 0.357070192
 
         if 0 <= w1 and w1 <= p1:
-            return (2*w1)/p1 - 1 + self.v.__next__() + self.w.__next__()
+            return (2 * w1) / p1 - 1 + self.v.__next__() + self.w.__next__()
 
-        if p1 < w1 and w1 <= (p1+p2):
-            return 1.5*((w1-p1)/p2 -1 + self.v.__next__())
+        if p1 < w1 and w1 <= (p1 + p2):
+            return 1.5 * ((w1 - p1) / p2 - 1 + self.v.__next__())
 
-        if (1-p4) < w1 and w1 <= 1:
+        if (1 - p4) < w1 and w1 <= 1:
             while True:
                 s1 = self.v.__next__()
                 s2 = self.w.__next__()
-                x = 4.5-math.log(s2,math.e)
-                if(x*s1*s1 > 4.5):
+                x = 4.5 - math.log(s2, math.e)
+                if (x * s1 * s1 > 4.5):
                     break
 
-            return (2*x)**(0.5) * numpy.sign(w1-(1-p4/2))
+            return (2 * x) ** (0.5) * numpy.sign(w1 - (1 - p4 / 2))
 
-        if p1+p2 < w1 and w1 <= (1-p4):
+        if p1 + p2 < w1 and w1 <= (1 - p4):
             while True:
                 w1 = self.u.__next__(-3, 3)
                 w2 = self.v.__next__()
                 v = abs(w1)
 
-                W = (c4/M)*(3-v)*(3-v)
+                W = (c4 / M) * (3 - v) * (3 - v)
                 S = 0
 
                 if v < 1.5:
-                    S = (c3/M) * (1.5 - v)
+                    S = (c3 / M) * (1.5 - v)
                 if v < 1:
-                    S = S + (c2/M)*(3-v*v)-W
+                    S = S + (c2 / M) * (3 - v * v) - W
 
-                if w2 > (c1/M) * math.exp(-v*v)/2 - S - W:
+                if w2 > (c1 / M) * math.exp(-v * v) / 2 - S - W:
                     break
 
             return w1
@@ -144,69 +140,120 @@ class Normal:
     def r2(self):  # rozklad normalny N(0, 1)
         while True:
             w1 = self.u.__next__()
-            w2 = self.v.__next__(-((2/math.e)**(0.5)),(2/math.e)**(0.5))
-            t = w2/w1
+            w2 = self.v.__next__(-((2 / math.e) ** 0.5), (2 / math.e) ** 0.5)
+            t = w2 / w1
 
-            if w1*w1 > math.exp(-t*t/2):
+            if w1 * w1 > math.exp(-t * t / 2):
                 break
         return t
 
 
+class Tests:
 
-# https://www.codespeedy.com/runs-test-of-randomness-in-python-programming/
-def runs_test(l, l_median): # testy serii
-    runs, n1, n2 = 0, 0, 0
+    # https://www.codespeedy.com/runs-test-of-randomness-in-python-programming/
+    def runs_test(l, l_median):  # testy serii
+        runs, n1, n2 = 0, 0, 0
 
-    for i in range(len(l)):
+        for i in range(len(l)):
+            # no. of runs
+            if (l[i] >= l_median and l[i - 1] < l_median) or (l[i] < l_median and l[i - 1] >= l_median):
+                runs += 1
 
-        # no. of runs
-        if (l[i] >= l_median and l[i - 1] < l_median) or (l[i] < l_median and l[i - 1] >= l_median):
-            runs += 1
+            if (l[i]) >= l_median:
+                n1 += 1
+            else:
+                n2 += 1
 
-        if (l[i]) >= l_median:
-            n1 += 1
+        runs_exp = ((2 * n1 * n2) / (n1 + n2)) + 1
+        stan_dev = math.sqrt((2 * n1 * n2 * (2 * n1 * n2 - n1 - n2)) /
+                             (((n1 + n2) ** 2) * (n1 + n2 - 1)))
 
-        else:
-            n2 += 1
+        z = (runs - runs_exp) / stan_dev
 
-    runs_exp = ((2 * n1 * n2) / (n1 + n2)) + 1
-    stan_dev = math.sqrt((2 * n1 * n2 * (2 * n1 * n2 - n1 - n2)) /
-                         (((n1 + n2) ** 2) * (n1 + n2 - 1)))
-
-    z = (runs - runs_exp) / stan_dev
-
-    return z
-
-
-g = RandomNumberGenerator()
-u = UniformNumberGenerator()
-b1 = Bernoulli()
-b2 = Binomial()
-p = Poisson()
-ex = Exponential()
-nor = Normal()
-a = []
-b = []
-c = []
-d = []
-e = []
-f = []
-n = []
-
-for i in range(1000):
-    a.append(g.__next__())
-    e.append(ex.run())
-    n.append(nor.r2())
+        return z
 
 
-print(c)
-print(a)
-print(b)
-print(d)
-print(e)
-print(f)
-print(n)
+class Histogram:
+    def __init__(self, n=10):
+        self.n = n
 
-plt.figure(figsize=(10,10))
-plt.hist(n, range=[1, 2])
-plt.show()
+    def draw_RNG(self):
+        r = RandomNumberGenerator()
+        l = []
+
+        for i in range(self.n):
+            l.append(r.__next__())
+
+        plt.title("Random Numbers")
+        plt.hist(l)
+        plt.show()
+
+    def draw_UN(self):
+        u = UniformNumberGenerator()
+        l = []
+
+        for i in range(self.n):
+            l.append(u.__next__())
+
+        plt.title("Uniform numbers")
+        plt.hist(l)
+        plt.show()
+
+    def draw_bernoulli(self, p):
+        b = Bernoulli()
+        l = []
+
+        for i in range(self.n):
+            l.append(b.__next__(p))
+
+        plt.title("Bernoulli")
+        plt.hist(l)
+        plt.show()
+
+    def draw_binomial(self, p, N):
+        b = Binomial()
+        l = []
+
+        for i in range(self.n):
+            l.append(b.run(p, N))
+
+        plt.title("Binomial")
+        plt.hist(l)
+        plt.show()
+
+    def draw_poisson(self, lamb):
+        p = Poisson()
+        l = []
+
+        for i in range(self.n):
+            l.append(p.run(lamb))
+
+        plt.title("Poisson")
+        plt.hist(l)
+        plt.show()
+
+    def draw_exponential(self):
+        e = Exponential()
+        l = []
+
+        for i in range(self.n):
+            l.append(e.run())
+
+        plt.title("Exponential")
+        plt.hist(l)
+        plt.show()
+
+    def draw_normal(self):
+        n = Normal()
+        l = []
+
+        for i in range(self.n):
+            l.append(n.r2())
+
+        plt.title("Normal")
+        plt.hist(l, range=[1, 2])
+        plt.show()
+
+
+h = Histogram(10000)
+h.draw_normal()
